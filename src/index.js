95e21e91731f2719b8310d5b93fdafa7b02d7131
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-28 11:37:44 -0600

    still setting up my enviorment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import './style.css';
 
 function checkingConnection() {
-  return console.log('We are connected!!');
+  return console.log('We are connected with the plugins!!');
 }
 
 checkingConnection();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDo',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
