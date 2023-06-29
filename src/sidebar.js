001a1e21de8c5216f6be381971857d90de774cf6
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-29 15:08:14 -0600

    making comments to map out the side bar

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,3 +5,17 @@ sidebar.classList.add('sidebar');
 mainContainer.appendChild(sidebar);
 
 sidebar.innerText = 'i am a sidebar';
+
+const overView = document.createElement('div');
+//My Today
+//next 7days
+//all my tasks
+
+const myLists = document.createElement('div');
+//personal
+//work
+//Grocery list
+//+ add list
+
+const ToDoProjects = document.createElement('div');
+// + add new project
