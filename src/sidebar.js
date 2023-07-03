63d70db623c8058429b299416c15cc3df0075c58
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-03 13:11:46 -0600

    hard coded the nav to simplify the js. now I only added the add links using js.

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,46 +1,20 @@
-const lists = document.getElementById('navLists');
+const myLists = document.getElementById('myLists');
+const toDoBoards = document.getElementById('ToDoBoards');
 
-const overView = document.createElement('ul');
-overView.id = 'overView';
-lists.appendChild(overView);
+const addListLine = document.createElement('li');
+addListLine.role = 'presentation';
+myLists.appendChild(addListLine);
 
-const myToday = document.createElement('li');
-myToday.innerText = 'My Today';
-overView.appendChild(myToday);
+const addListLink = document.createElement('a');
+addListLink.innerText = 'Add List';
+addListLink.href = ' ';
+addListLine.appendChild(addListLink);
 
-const next7Days = document.createElement('li');
-next7Days.innerText = 'Next 7 Days';
-overView.appendChild(next7Days);
+const addToDoBoardLine = document.createElement('li');
+addToDoBoardLine.role = 'presentation';
+toDoBoards.appendChild(addToDoBoardLine);
 
-const allTasks = document.createElement('li');
-allTasks.innerText = 'All Tasks';
-overView.appendChild(allTasks);
-
-const myLists = document.createElement('ul');
-myLists.id = 'myLists';
-myLists.innerText = 'My Lists';
-lists.appendChild(myLists);
-
-const personalList = document.createElement('li');
-personalList.innerText = 'Personal';
-myLists.appendChild(personalList);
-
-const workList = document.createElement('li');
-workList.innerText = 'Work';
-myLists.appendChild(workList);
-
-const groceryList = document.createElement('li');
-groceryList.innerText = 'Grocery List';
-myLists.appendChild(groceryList);
-
-const addList = document.createElement('li');
-addList.innerText = 'Add List';
-myLists.appendChild(addList);
-
-const ToDoBoards = document.createElement('ul');
-ToDoBoards.innerText = 'To Do Boards';
-lists.appendChild(ToDoBoards);
-
-const addToDoBoard = document.createElement('li');
-addToDoBoard.innerText = 'Add New Board';
-ToDoBoards.appendChild(addToDoBoard);
+const addToDoBoardLink = document.createElement('a');
+addToDoBoardLink.innerText = 'Add Board';
+addToDoBoardLink.href = ' ';
+addToDoBoardLine.appendChild(addToDoBoardLink);
