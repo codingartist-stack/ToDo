367b325a2c2e8ddfa6b1b541851c0042d4ae67eb
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-03 09:35:23 -0600

    when I created the skip nav via js it was being put at the bottom of the content. hard coded it to the template. will probaby adjust the link as I go.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import _ from 'lodash';
 import './style.css';
 import './sidebar';
 import './displayContainer';
-import './skip-nav';
--- src/skip-nav.js
+++
@@ -1,7 +0,0 @@
-import { mainContentContainer } from './displayContainer';
-
-const skipNav = document.createElement('a');
-skipNav.innerText = 'skip navigation';
-skipNav.href = mainContentContainer;
-
-document.body.appendChild(skipNav);
