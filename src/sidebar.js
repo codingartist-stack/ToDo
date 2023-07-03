fbba3347ae8d8ae71db1d746ba4b95ca46d86e9a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-03 11:31:18 -0600

    added the overview items using js to start creating the side bar navigation.

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,17 +1,26 @@
-const list = document.getElementById('nav');
+const lists = document.getElementById('navLists');
 
-const overView = document.createElement('li');
-overView.innerText = 'My Today';
-list.appendChild(overView);
-//My Today
-//next 7days
-//all my tasks
+const overView = document.createElement('ul');
+overView.id = 'overView';
+lists.appendChild(overView);
 
-const myLists = document.createElement('div');
+const myToday = document.createElement('li');
+myToday.innerText = 'My Today';
+lists.appendChild(myToday);
+
+const next7Days = document.createElement('li');
+next7Days.innerText = 'Next 7 Days';
+lists.appendChild(next7Days);
+
+const allTasks = document.createElement('li');
+allTasks.innerText = 'All Tasks';
+lists.appendChild(allTasks);
+
+const myLists = document.createElement('ul');
 //personal
 //work
 //Grocery list
 //+ add list
 
-const ToDoProjects = document.createElement('div');
+const ToDoProjects = document.createElement('ul');
 // + add new project
