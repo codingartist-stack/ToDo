c3d57d195dfed411f1be07ee90f1528b73466f0f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-03 15:08:14 -0600

    adjusted the nesting for the sidebar list. added image to use as a background.

--- src/sidebar.js
+++ src/sidebar.js
@@ -9,7 +9,10 @@ addListLink.innerText = 'Add List';
 addListLink.href = ' ';
 addListLine.appendChild(addListLink);
 
-const addToDoBoardLine = document.createElement('ul');
+const addToDoBoardLists = document.createElement('ul');
+toDoBoards.appendChild(addToDoBoardLists);
+
+const addToDoBoardLine = document.createElement('li');
 toDoBoards.appendChild(addToDoBoardLine);
 
 const addToDoBoardLink = document.createElement('a');
