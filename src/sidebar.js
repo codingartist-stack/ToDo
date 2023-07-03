9f9005f15670ab86b11c9d65a4acffd20a7c0248
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-03 12:35:38 -0600

    created the list of the sidebar. I hope it makes sense.

--- src/sidebar.js
+++ src/sidebar.js
@@ -6,21 +6,41 @@ lists.appendChild(overView);
 
 const myToday = document.createElement('li');
 myToday.innerText = 'My Today';
-lists.appendChild(myToday);
+overView.appendChild(myToday);
 
 const next7Days = document.createElement('li');
 next7Days.innerText = 'Next 7 Days';
-lists.appendChild(next7Days);
+overView.appendChild(next7Days);
 
 const allTasks = document.createElement('li');
 allTasks.innerText = 'All Tasks';
-lists.appendChild(allTasks);
+overView.appendChild(allTasks);
 
 const myLists = document.createElement('ul');
-//personal
-//work
-//Grocery list
-//+ add list
+myLists.id = 'myLists';
+myLists.innerText = 'My Lists';
+lists.appendChild(myLists);
 
-const ToDoProjects = document.createElement('ul');
-// + add new project
+const personalList = document.createElement('li');
+personalList.innerText = 'Personal';
+myLists.appendChild(personalList);
+
+const workList = document.createElement('li');
+workList.innerText = 'Work';
+myLists.appendChild(workList);
+
+const groceryList = document.createElement('li');
+groceryList.innerText = 'Grocery List';
+myLists.appendChild(groceryList);
+
+const addList = document.createElement('li');
+addList.innerText = 'Add List';
+myLists.appendChild(addList);
+
+const ToDoBoards = document.createElement('ul');
+ToDoBoards.innerText = 'To Do Boards';
+lists.appendChild(ToDoBoards);
+
+const addToDoBoard = document.createElement('li');
+addToDoBoard.innerText = 'Add New Board';
+ToDoBoards.appendChild(addToDoBoard);
