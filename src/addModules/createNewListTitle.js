aac7318d339edc239722057d2c98fb77aed69d35
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-05 14:05:18 -0600

    tried to add a section title to the main content container when added the card. When I did one case it worked, but when I tried to add a switch statement to say which section to add it to it added the card, but not the section container.

--- src/addModules/createNewListTitle.js
+++ src/addModules/createNewListTitle.js
@@ -1,3 +1,6 @@
+const myListsContainer = document.getElementById('myListsContainer');
+const ToDoBoardContainer = document.getElementById('ToDoBoardContainer');
+
 export default function createNewListTitle(event) {
   const newLine = document.createElement('li');
   const linkTag = document.createElement('a');
@@ -7,4 +10,23 @@ export default function createNewListTitle(event) {
   const lineTitle = prompt('List Title');
   linkTag.innerText = lineTitle;
   event.currentTarget.parentNode.insertBefore(newLine, event.currentTarget);
+
+  const sectionContainer = document.createElement('div');
+  const sectionTitle = document.createElement('h3');
+  sectionTitle.innerText = lineTitle;
+
+  sectionContainer.appendChild(sectionTitle);
+
+  switch (event.currentTarget) {
+    case 'li#myListButton':
+      myListsContainer.appendChild(sectionContainer);
+      break;
+
+    case 'li#addBoardButton':
+      ToDoBoardContainer.appendChild(sectionContainer);
+      break;
+
+    default:
+      break;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ import './addModules/createNewListTitle';
 import createNewListTitle from './addModules/createNewListTitle';
 import createCard from './addModules/createNewCard';
 
+const myListsContainer = document.getElementById('myListsContainer');
 const addMyListButton = document.getElementById('myListButton');
 const addBoard = document.getElementById('addBoardButton');
 const personalContainer = document.getElementById('personalContainer');
 const workContainer = document.getElementById('workContainer');
 const shoppingListContainer = document.getElementById('shoppingListContainer');
+const ToDoBoardContainer = document.getElementById('ToDoBoardContainer');
 
 addMyListButton.addEventListener('click', (currentTarget) => {
   createNewListTitle(currentTarget);
+  createCard(myListsContainer);
 });
 
 addBoard.addEventListener('click', (currentTarget) => {
   createNewListTitle(currentTarget);
+  createCard(ToDoBoardContainer);
 });
 
 createCard(personalContainer);
