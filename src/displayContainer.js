f8d1a6b54b67104dfe49e393249d8639780dbab3
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-05 10:54:26 -0600

    hardcoded containers. So I can add cards to the containers. I don't know if this will work, but we are going to try it.

--- src/displayContainer.js
+++ src/displayContainer.js
@@ -1,3 +1,8 @@
 export const mainContentContainer = document.getElementById(
   'mainContentContainer'
 );
+
+//create a card with the new title. in the content container
+// be able to add more todos with a check mark.
+
+//once all items checked pop up a congrates
