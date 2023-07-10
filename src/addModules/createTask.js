7723df6dea47b1c9d5a288abe2ef7216054aabe0
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-10 16:29:17 -0600

    moved the checkbox to the front.

--- src/addModules/createTask.js
+++ src/addModules/createTask.js
@@ -4,11 +4,12 @@ export default function createNewTask(button) {
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   const para = document.createElement('p');
+  para.appendChild(checkbox);
   para.innerText =
     button.parentElement.childNodes[0].value +
     ' By: ' +
     button.parentElement.childNodes[2].value;
-  para.appendChild(checkbox);
+
   const parentDiv = button.parentNode;
   parentDiv.parentNode.insertBefore(para, parentDiv);
 
