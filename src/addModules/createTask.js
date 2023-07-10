7ab2984c0349c46382f893c7268787386894126a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-10 16:28:36 -0600

    figured out how to add text to paragraph

--- src/addModules/createTask.js
+++ src/addModules/createTask.js
@@ -1,8 +1,17 @@
 const taskFieldEntry = document.getElementsByClassName('fieldEntry');
 
 export default function createNewTask(button) {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
   const para = document.createElement('p');
-  //   para.innerText = textEntry;
+  para.innerText =
+    button.parentElement.childNodes[0].value +
+    ' By: ' +
+    button.parentElement.childNodes[2].value;
+  para.appendChild(checkbox);
   const parentDiv = button.parentNode;
   parentDiv.parentNode.insertBefore(para, parentDiv);
+
+  button.parentElement.childNodes[0].value = '';
+  button.parentElement.childNodes[2].value = '';
 }
