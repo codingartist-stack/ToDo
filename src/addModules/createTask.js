760c6e885367046811f38a05a5c02d4baae51f66
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-10 16:30:25 -0600

    moved the checkbox back because it didn't show up.

--- src/addModules/createTask.js
+++ src/addModules/createTask.js
@@ -4,13 +4,15 @@ export default function createNewTask(button) {
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   const para = document.createElement('p');
-  para.appendChild(checkbox);
+
   para.innerText =
     button.parentElement.childNodes[0].value +
     ' By: ' +
     button.parentElement.childNodes[2].value;
 
   const parentDiv = button.parentNode;
+
+  para.appendChild(checkbox);
   parentDiv.parentNode.insertBefore(para, parentDiv);
 
   button.parentElement.childNodes[0].value = '';
