58cddd14dd19eeb7948f89f95e6a8a7d9148eebd
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-18 15:55:41 -0600

    deleted most of the code to start over." "

--- src/displayContainer.js
+++
@@ -1,8 +0,0 @@
-export const mainContentContainer = document.getElementById(
-  'mainContentContainer'
-);
-
-//create a card with the new title. in the content container
-// be able to add more todos with a check mark.
-
-//once all items checked pop up a congrates
