b325a8b66732160200acd56ceb8551008a02032f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-18 20:30:16 -0600

    added another constructor to the toDo item

--- src/toDoListClass.js
+++ src/toDoListClass.js
@@ -1,17 +1,8 @@
-export default class toDoList {
-  constructor(
-    toDoSection,
-    toDoListTitle,
-    toDoItem1,
-    toDoItem2,
-    toDoItem3,
-    toDoItem4
-  ) {
+export default class toDoItem {
+  constructor(toDoSection, toDoList, toDoItem, dueDate) {
     this.toDoSection = toDoSection;
-    this.toDoListTitle = toDoListTitle;
-    this.toDoItem1 = toDoItem1;
-    this.toDoItem2 = toDoItem2;
-    this.toDoItem3 = toDoItem3;
-    this.toDoItem4 = toDoItem4;
+    this.toDoList = toDoList;
+    this.toDoItem = toDoItem;
+    this.dueDate = dueDate;
   }
 }
