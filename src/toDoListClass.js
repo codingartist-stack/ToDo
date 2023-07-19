276821771b85e954627528bbfc5521ef7071fb13
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-18 18:57:45 -0600

    created a toDo list class I don't know if that is the best approach.

---
+++ src/toDoListClass.js
@@ -0,0 +1,17 @@
+export default class toDoList {
+  constructor(
+    toDoSection,
+    toDoListTitle,
+    toDoItem1,
+    toDoItem2,
+    toDoItem3,
+    toDoItem4
+  ) {
+    this.toDoSection = toDoSection;
+    this.toDoListTitle = toDoListTitle;
+    this.toDoItem1 = toDoItem1;
+    this.toDoItem2 = toDoItem2;
+    this.toDoItem3 = toDoItem3;
+    this.toDoItem4 = toDoItem4;
+  }
+}
